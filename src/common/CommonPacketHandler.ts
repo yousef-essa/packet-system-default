c0c76a58b87146a5d30f0ac2ceb46eaf34cbd116
yousef-essa <yousef.essa.p@gmail.com>
2022-06-29 00:19:37 +0200

    Force packetAdapter to return a single instance

--- src/common/CommonPacketHandler.ts
+++ src/common/CommonPacketHandler.ts
@@ -2,6 +2,20 @@ import CommonPacketAdapter from "./CommonPacketAdapter";
 import {PacketHandler} from "packet-system";
 
 export default abstract class CommonPacketHandler<T extends CommonPacketAdapter<any>> extends PacketHandler {
+    private readonly _packetAdapter: T
+
+    protected constructor(options = {}) {
+        super(options);
+        this._packetAdapter = this.packetAdapter()
+
+        // a hacky way to override the adapter method
+        // to return a single instance of this packet
+        // adapter
+        this.packetAdapter = () => {
+            return this._packetAdapter
+        }
+    }
+
     /**
      * Initializes the connection by invoking the
      * {@link CommonPacketAdapter#connect} method - if applicable.
